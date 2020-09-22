faf92ac46d056d8b23a8c584e2cae8d16082d371
Tuan Nguyen Duc <tuan@umbala.io>
2020-09-22 16:17:48 +0700

    chore: update client interface

--- flipperandroidnoop/src/main/java/com/facebook/flipper/core/FlipperClient.kt
+++ flipperandroidnoop/src/main/java/com/facebook/flipper/core/FlipperClient.kt
@@ -1,11 +1,27 @@
 package com.facebook.flipper.core
 
 class FlipperClient {
-    fun addPlugin(plugin: Any? = null) {
-        // No-op
+    fun addPlugin(plugin: Any?) {
+
+    }
+
+    fun getPlugin(id: String?): Any? {
+        return null;
+    }
+
+    fun removePlugin(plugin: Any?) {
+
     }
 
     fun start() {
-        // No-op
+
+    }
+
+    fun stop() {
+
+    }
+
+    fun unsubscribe() {
+
     }
 }
